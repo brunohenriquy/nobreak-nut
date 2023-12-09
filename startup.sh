1d07d21a5d4783beb7562b7ce068295f514d45b0
Bruno Henrique <bruno.henriquy@gmail.com>
2023-12-09 19:43:20 -0300

    Adjusting start up

--- startup.sh
+++ startup.sh
@@ -1,5 +1,17 @@
 #! /usr/bin/bash
 
-/lib/nut/blazer_ser -a ups || true
-/etc/init.d/nut-server restart || true
-upsc ups || true
+# Run custom commands after container startup
+/lib/nut/blazer_ser -a ups
+if [ $? -ne 0 ]; then
+    echo "Error running /lib/nut/blazer_ser"
+fi
+
+/etc/init.d/nut-server restart
+if [ $? -ne 0 ]; then
+    echo "Error restarting nut-server"
+fi
+
+upsc ups
+if [ $? -ne 0 ]; then
+    echo "Error running upsc"
+fi
