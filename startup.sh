73da932ef5c9c9e5081198efe9fe9916e9a88803
Bruno Henrique <bruno.henriquy@gmail.com>
2023-12-10 03:52:24 -0300

    Adjustments

--- startup.sh
+++ startup.sh
@@ -2,6 +2,7 @@
 
 # Run custom commands after container startup
 
+echo "Running 1/3"
 #/lib/nut/blazer_ser -a tsshara
 #if [ $? -ne 0 ]; then
 #    echo "Error running /lib/nut/blazer_ser"
@@ -11,18 +12,29 @@
 if [ $? -ne 0 ]; then
     echo "Error running /lib/nut/nutdrv_qx"
 fi
+echo ""
+echo ""
+echo ""
 
+echo "Running 2/3"
 /etc/init.d/nut-server restart
 if [ $? -ne 0 ]; then
     echo "Error restarting nut-server"
 fi
+echo ""
+echo ""
+echo ""
 
 #sleep 10
 
+echo "Running 3/3"
 upsc tsshara
 if [ $? -ne 0 ]; then
     echo "Error running upsc"
 fi
+echo ""
+echo ""
+echo ""
 
 while true; do
   sleep 3600
